1c883f42fa4e9f26c5a94191e0d69eab1521b7b0
lorenwel <lorenwel@ethz.ch>
2021-04-14 16:45:08 +0200

    Named variables

--- elevation_mapping_cupy/script/custom_kernels.py
+++ elevation_mapping_cupy/script/custom_kernels.py
@@ -165,10 +165,13 @@ def add_points_kernel(resolution, width, height, sensor_noise_factor,
                     U nmap_trav = map[get_map_idx(nidx, 3)];
                     // Time layer
                     U non_updated_t = map[get_map_idx(nidx, 4)];
+                    // upper bound
+                    U nmap_upper = map[get_map_idx(nidx, 5)];
+                    U nmap_is_upper = map[get_map_idx(nidx, 6)];
 
                     // If invalid, do upper bound check, then skip
                     if (nmap_valid < 0.5) {
-                      if (nz < map[get_map_idx(nidx, 5)] || map[get_map_idx(nidx, 6)] < 0.5) {
+                      if (nz < nmap_upper || nmap_is_upper < 0.5) {
                         atomicAdd(&map[get_map_idx(nidx, 5)], nz);
                         atomicAdd(&map[get_map_idx(nidx, 6)], 1.0f);
                       }
