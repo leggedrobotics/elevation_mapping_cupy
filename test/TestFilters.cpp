68b5308ac321b144e170305bbcaab49eb405e0b1
fabianje <fabian.jenelten@mavt.ethz.ch>
2021-06-07 11:21:38 +0200

    check against original map.

--- test/TestFilters.cpp
+++ test/TestFilters.cpp
@@ -15,6 +15,7 @@ TEST(TestInpainting, initialization) {  // NOLINT
   GridMap map;
   map.setGeometry(Length(1.0, 2.0), 0.1, Position(0.0, 0.0));
   map.add("elevation", 1.0);
+  const Eigen::MatrixXf H0 = map.get("elevation");
   Eigen::MatrixXf& H_in = map.get("elevation");
 
   // Set nan patches.
@@ -27,11 +28,12 @@ TEST(TestInpainting, initialization) {  // NOLINT
   inpainting::biLinearInterpolation(map, "elevation", "filled_bilinear");
 
   // Check if open-cv in-painting was successful.
+  constexpr double threshold = 1.0e-9;
   const Eigen::MatrixXf& H_out_ref = map.get("filled_nonlin");
   EXPECT_FALSE(std::isnan(H_out_ref.norm()));
+  EXPECT_TRUE((H_out_ref - H0).norm() < threshold);
 
   // Compare against open-cv in-painting.
-  constexpr double threshold = 1.0e-9;
   EXPECT_TRUE((H_out_ref - map.get("filled_min")).norm() < threshold);
   EXPECT_TRUE((H_out_ref - map.get("filled_bilinear")).norm() < threshold);
 }
