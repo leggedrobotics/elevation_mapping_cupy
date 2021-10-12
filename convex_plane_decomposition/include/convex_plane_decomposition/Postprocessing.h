a759aa907e7f3ba44a22c5b1efefff0b336eb161
rgrandia <rubengrandia@gmail.com>
2021-10-12 15:41:06 +0200

    add a smooth layer to be used as base reference

--- convex_plane_decomposition/include/convex_plane_decomposition/Postprocessing.h
+++ convex_plane_decomposition/include/convex_plane_decomposition/Postprocessing.h
@@ -13,6 +13,15 @@ struct PostprocessingParameters {
 
   /// Size of the kernel creating the boundary offset. In number of pixels.
   int nonplanar_horizontal_offset = 2;
+
+  /// Half the width of the dilation used before the smooth layer [m]
+  double smoothing_dilation_size = 0.2;
+
+  /// Half the width of the box kernel used for the smooth layer [m]
+  double smoothing_box_kernel_size = 0.3;
+
+  /// Half the width of the Gaussian kernel used for the smooth layer [m]
+  double smoothing_gauss_kernel_size = 0.1;
 };
 
 class Postprocessing {
@@ -30,6 +39,7 @@ class Postprocessing {
   void dilationInNonplanarRegions(Eigen::MatrixXf& elevationData, const Eigen::MatrixXf& planarityMask) const;
   void addHeightOffset(Eigen::MatrixXf& elevationData, const Eigen::MatrixXf& planarityMask) const;
   void addHeightOffset(std::vector<PlanarRegion>& planarRegions) const;
+  void addSmoothLayer(grid_map::GridMap& gridMap, const Eigen::MatrixXf& elevationData, const Eigen::MatrixXf& planarityMask) const;
 
   PostprocessingParameters parameters_;
 };
