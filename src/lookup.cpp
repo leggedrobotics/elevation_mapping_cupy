7a4370bad0cfa615b3cb677ae68266591ab1bbc6
rgrandia <rubengrandia@gmail.com>
2021-10-15 16:37:05 +0200

    implement helper to get all values from line iterator

--- src/lookup.cpp
+++ src/lookup.cpp
@@ -41,5 +41,34 @@ LookupResult maxValueBetweenLocations(const grid_map::Position& position1, const
   return {maxValueFound, searchMaxValue, maxPosition};
 }
 
+std::vector<grid_map::Position3> valuesBetweenLocations(const grid_map::Position& position1, const grid_map::Position& position2,
+                                                        const grid_map::GridMap& gridMap, const grid_map::Matrix& data) {
+  // Map corner points into grid map. The line iteration doesn't account for the case where the line does not intersect the map.
+  const grid_map::Position startPos = gridMap.getClosestPositionInMap(position1);
+  const grid_map::Position endPos = gridMap.getClosestPositionInMap(position2);
+
+  // Approximate amount of points to reserve memory
+  const auto manhattanDistance = std::max(std::abs(endPos.x() - startPos.x()), std::abs(endPos.y() - startPos.y()));
+  const int manhattanPixels = std::ceil(manhattanDistance / gridMap.getResolution()) + 1;
+
+  // Container for results
+  std::vector<grid_map::Position3> lineValues;
+  lineValues.reserve(manhattanPixels);
+
+  // Line iteration
+  for (grid_map::LineIterator iterator(gridMap, startPos, endPos); !iterator.isPastEnd(); ++iterator) {
+    const grid_map::Index index = *iterator;
+    const auto value = data(index(0), index(1));
+
+    if (std::isfinite(value)) {
+      grid_map::Position position;
+      gridMap.getPosition(index, position);
+      lineValues.push_back({position.x(), position.y(), value});
+    }
+  }
+
+  return lineValues;
+}
+
 }  // namespace lookup
 }  // namespace grid_map
