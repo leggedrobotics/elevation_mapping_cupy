94a662b6fea0dc8aec3069a3549689ccc658bacb
rgrandia <rubengrandia@gmail.com>
2022-02-02 16:33:09 +0100

    set max iterations in region growing and print if it is reached. Remove unnecessary memory allocations

--- convex_plane_decomposition/src/ConvexRegionGrowing.cpp
+++ convex_plane_decomposition/src/ConvexRegionGrowing.cpp
@@ -11,6 +11,7 @@ namespace convex_plane_decomposition {
 CgalPolygon2d createRegularPolygon(const CgalPoint2d& center, double radius, int numberOfVertices) {
   assert(numberOfVertices > 2);
   CgalPolygon2d polygon;
+  polygon.container().reserve(numberOfVertices);
   double angle = (2. * M_PI) / numberOfVertices;
   for (int i = 0; i < numberOfVertices; ++i) {
     double phi = i * angle;
@@ -58,13 +59,14 @@ std::array<CgalPoint2d, 4> get2ndNeighbours(const CgalPolygon2d& polygon, int i)
 
 bool remainsConvexWhenMovingPoint(const CgalPolygon2d& polygon, int i, const CgalPoint2d& point) {
   auto secondNeighbours = get2ndNeighbours(polygon, i);
-  CgalPolygon2d subPolygon;
-  subPolygon.container().reserve(5);
-  subPolygon.push_back(secondNeighbours[0]);
-  subPolygon.push_back(secondNeighbours[1]);
-  subPolygon.push_back(point);
-  subPolygon.push_back(secondNeighbours[2]);
-  subPolygon.push_back(secondNeighbours[3]);
+  using CgalPolygon2dFixedSize = CGAL::Polygon_2<K, std::array<K::Point_2, 5>>;
+
+  CgalPolygon2dFixedSize subPolygon;
+  subPolygon.container()[0] = secondNeighbours[0];
+  subPolygon.container()[1] = secondNeighbours[1];
+  subPolygon.container()[2] = point;
+  subPolygon.container()[3] = secondNeighbours[2];
+  subPolygon.container()[4] = secondNeighbours[3];
   return subPolygon.is_convex();
 }
 
@@ -99,18 +101,47 @@ bool pointCanBeMoved(const CgalPolygon2d& growthShape, int i, const CgalPoint2d&
   }
 }
 
+namespace {
+inline std::ostream& operator<<(std::ostream& os, const CgalPolygon2d& p) {
+  os << "CgalPolygon2d: \n";
+  for (auto it = p.vertices_begin(); it != p.vertices_end(); ++it) {
+    os << "\t(" << it->x() << ", " << it->y() << ")\n";
+  }
+  return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const CgalPolygonWithHoles2d& p) {
+  os << "CgalPolygonWithHoles2d: \n";
+  os << "\t" << p.outer_boundary() << "\n";
+  os << "\tHoles: \n";
+  for (auto it = p.holes_begin(); it != p.holes_end(); ++it) {
+    os << "\t\t" << *it << "\n";
+  }
+  return os;
+}
+}  // namespace
+
 template <typename T>
 CgalPolygon2d growConvexPolygonInsideShape_impl(const T& parentShape, CgalPoint2d center, int numberOfVertices, double growthFactor) {
+  const auto centerCopy = center;
   constexpr double initialRadiusFactor = 0.999;
+  constexpr int maxIter = 1000;
   double radius = initialRadiusFactor * distance(center, parentShape);
+
   CgalPolygon2d growthShape = createRegularPolygon(center, radius, numberOfVertices);
+
+  if (radius == 0.0) {
+    std::cerr << "[growConvexPolygonInsideShape] Zero initial radius. Provide a point with a non-zero offset to the boundary.\n";
+    return growthShape;
+  }
 
   // Cached values per vertex
   std::vector<bool> blocked(numberOfVertices, false);
   std::vector<CgalCircle2d> freeSpheres(numberOfVertices, CgalCircle2d(center, radius * radius));
 
   int Nblocked = 0;
-  while (Nblocked < numberOfVertices) {
+  int iter = 0;
+  while (Nblocked < numberOfVertices && iter < maxIter) {
     for (int i = 0; i < numberOfVertices; i++) {
       if (!blocked[i]) {
         const auto candidatePoint = getPointOnLine(center, growthShape.vertex(i), growthFactor);
@@ -123,7 +154,17 @@ CgalPolygon2d growConvexPolygonInsideShape_impl(const T& parentShape, CgalPoint2
         }
       }
     }
+    ++iter;
   }
+
+  if (iter == maxIter) {
+    std::cerr << "[growConvexPolygonInsideShape] max iteration in region growing! Debug information: \n";
+    std::cerr << "numberOfVertices: " << numberOfVertices << "\n";
+    std::cerr << "growthFactor: " << growthFactor << "\n";
+    std::cerr << "Center: " << centerCopy.x() << ", " << centerCopy.y() << "\n";
+    std::cerr << parentShape << "\n";
+  }
+
   return growthShape;
 }
 
