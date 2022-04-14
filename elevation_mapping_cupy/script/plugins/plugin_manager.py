cb0b37b97d29c2188dd52d532656ec083864cd09
Takahiro <takahiro.miki1992@gmail.com>
2022-04-14 13:48:14 +0200

    Cleaned up checking plugins.

--- elevation_mapping_cupy/script/plugins/plugin_manager.py
+++ elevation_mapping_cupy/script/plugins/plugin_manager.py
@@ -62,7 +62,9 @@ class PluginManger(object):
         for param, extra_param in zip(plugin_params, extra_params):
             m = importlib.import_module("." + param.name, package="plugins") # -> 'module'
             for name, obj in inspect.getmembers(m):
-                if inspect.isclass(obj) and name not in ["PluginBase", "List"]:
+                if (inspect.isclass(obj) and
+                        issubclass(obj, PluginBase) and
+                        name != "PluginBase"):
                     # Add cell_n to params
                     extra_param["cell_n"] = self.cell_n
                     self.plugins.append(obj(**extra_param))
