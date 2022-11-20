fixed quaternion convention and gridmap transpose

elevation_mapping_cupy/script/elevation_mapping_cupy/elevation_mapping_ros.py
@@ -2,7 +2,7 @@ from elevation_mapping_cupy import ElevationMap
 from elevation_mapping_cupy import Parameter
 
 # General
-import os 
+import os
 import numpy as np
 
 # ROS
@@ -18,13 +18,27 @@ from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import MultiArrayLayout as MAL
 from std_msgs.msg import MultiArrayDimension as MAD
 
-class ElevationMapWrapper():
+import numpy as np
+
+PDC_DATATYPE = {
+    "1": np.int8,
+    "2": np.uint8,
+    "3": np.int16,
+    "4": np.uint16,
+    "5": np.int32,
+    "6": np.uint32,
+    "7": np.float32,
+    "8": np.float64,
+}
+
+
+class ElevationMapWrapper:
     def __init__(self):
         rospack = rospkg.RosPack()
         self.root = rospack.get_path("elevation_mapping_cupy")
         weight_file = os.path.join(self.root, "config/weights.dat")
         plugin_config_file = os.path.join(self.root, "config/plugin_config.yaml")
-        self.param = Parameter( use_chainer=False, weight_file=weight_file, plugin_config_file=plugin_config_file )
+        self.param = Parameter(use_chainer=False, weight_file=weight_file, plugin_config_file=plugin_config_file)
         self.node_name = "elevation_mapping"
         # ROS
         self.initalize_ros()
@@ -33,6 +47,8 @@ class ElevationMapWrapper():
         self.register_publishers()
         self.register_timers()
 
+        self._last_t = None
+
     def initalize_elevation_mapping(self):
         self.param.update()
         self._pointcloud_process_counter = 0
@@ -40,20 +56,22 @@ class ElevationMapWrapper():
         self._map_data = np.zeros((self._map.cell_n - 2, self._map.cell_n - 2), dtype=np.float32)
         self._map_q = None
         self._map_t = None
-        
+
     def initalize_ros(self):
         rospy.init_node(self.node_name, anonymous=False)
         self._tf_buffer = tf2_ros.Buffer()
         self._listener = tf2_ros.TransformListener(self._tf_buffer)
         self.get_ros_params()
-        
+
     def register_subscribers(self):
         pointcloud_subs = {}
         additional = []
         fusion = []
         for key, config in self.subscribers.items():
-            pointcloud_subs[key] = rospy.Subscriber(config["topic_name"], PointCloud2, self.pointcloud_callback, (config["channels"], config["fusion"]))
-            for chan,fus in zip(config["channels"],config["fusion"]):
+            pointcloud_subs[key] = rospy.Subscriber(
+                config["topic_name"], PointCloud2, self.pointcloud_callback, (config["channels"], config["fusion"])
+            )
+            for chan, fus in zip(config["channels"], config["fusion"]):
                 if chan not in additional:
                     additional.append(chan)
                     fusion.append(fus)
@@ -74,13 +92,13 @@ class ElevationMapWrapper():
         for k, v in self.publishers.items():
             print(f"Register Publisher: {k}")
             self._publishers[k] = rospy.Publisher(f"/{self.node_name}/{k}", GridMap, queue_size=10)
-            self._publishers_timers.append( rospy.Timer(rospy.Duration(1/v["fps"]), lambda x: self.publish_map(x, k)))
-            
+            self._publishers_timers.append(rospy.Timer(rospy.Duration(1 / v["fps"]), lambda x: self.publish_map(x, k)))
+
     def publish_map(self, t, k):
-        print(k)
+        print("Publish map")
         if self._map_q is None:
             return
-        
+
         gm = GridMap()
         gm.info.header.frame_id = self.map_frame
         gm.info.header.stamp = rospy.Time.now()
@@ -90,11 +108,11 @@ class ElevationMapWrapper():
         gm.info.length_y = self._map.map_length
         gm.info.pose.position.x = self._map_t.x
         gm.info.pose.position.y = self._map_t.y
-        gm.info.pose.position.z = self._map_t.z
-        gm.info.pose.orientation.w = 1 # self._map_q.w
-        gm.info.pose.orientation.x = 0 # self._map_q.x
-        gm.info.pose.orientation.y = 0 # self._map_q.y
-        gm.info.pose.orientation.z = 0 # self._map_q.z
+        gm.info.pose.position.z = 0.0
+        gm.info.pose.orientation.w = 1.0  # self._map_q.w
+        gm.info.pose.orientation.x = 0.0  # self._map_q.x
+        gm.info.pose.orientation.y = 0.0  # self._map_q.y
+        gm.info.pose.orientation.z = 0.0  # self._map_q.z
         gm.layers = self.publishers[k]["layers"]
         gm.basic_layers = self.publishers[k]["basic_layers"]
 
@@ -103,72 +121,89 @@ class ElevationMapWrapper():
         for i, layer in enumerate(gm.layers):
             self._map.get_map_with_name_ref(layer, self._map_data)
             # self._map_data = np.nan_to_num(self._map_data, False, 0)
-            data = self._map_data.copy() 
+            data = self._map_data.copy()
             arr = Float32MultiArray()
             arr.layout = MAL()
             N = self._map_data.shape[0]
-            arr.layout.dim.append( MAD(label="column_index",size=N,stride=int(N*N)))
-            arr.layout.dim.append( MAD(label="row_index",size=N,stride=N))
-            arr.data =  tuple(np.ascontiguousarray(data).reshape(-1))
-            gm.data.append( arr )
-        
-        self._publishers[k].publish( gm )
-    
+            arr.layout.dim.append(MAD(label="column_index", size=N, stride=int(N * N)))
+            arr.layout.dim.append(MAD(label="row_index", size=N, stride=N))
+            arr.data = tuple(np.ascontiguousarray(data.T).reshape(-1))
+            gm.data.append(arr)
+        gm.outer_start_index = 0
+        gm.inner_start_index = 0
+
+        self._publishers[k].publish(gm)
+
     def register_timers(self):
-        self.timer_variance = rospy.Timer(rospy.Duration(1/self.update_variance_fps), self.update_variance)
-        self.timer_pose = rospy.Timer(rospy.Duration(1/self.update_pose_fps), self.update_pose)
+        self.timer_variance = rospy.Timer(rospy.Duration(1 / self.update_variance_fps), self.update_variance)
+        self.timer_pose = rospy.Timer(rospy.Duration(1 / self.update_pose_fps), self.update_pose)
         self.timer_time = rospy.Timer(rospy.Duration(self.time_interval), self.update_time)
-        
+
     def pointcloud_callback(self, msg, config):
         # convert pcd into numpy array
-        channels = ['x','y','z']+config[0]
+        # output = {}
+        # total_bytes = msg.fields[-1].offset + PDC_DATATYPE[str(msg.fields[-1].datatype)](1).nbytes
+        # res = np.frombuffer(msg.data, np.dtype(np.uint8)).reshape((-1, msg.point_step))
+        # for field in msg.fields:
+        #     assert field.count == 1, "If this is not case, maybe does not work"
+        #     PDC_DATATYPE[str(field.datatype)]
+        #     dtype = PDC_DATATYPE[str(field.datatype)]
+        #     nbytes = dtype(1).nbytes
+        #     data = res[:, field.offset : field.offset + nbytes]
+        #     output[field.name] = np.frombuffer(data.copy(), np.dtype(dtype)).reshape((-1))
+        channels = ["x", "y", "z"] + config[0]
         fusion = config[1]
         points = ros_numpy.numpify(msg)
-        pts = np.empty((points.shape[0],0))
+        pts = np.empty((points.shape[0], 0))
         for ch in channels:
-            pts = np.append(pts,np.expand_dims(points[ch],1),axis=1)
+            pts = np.append(pts, np.expand_dims(points[ch], 1), axis=1)
 
         # get pose of pointcloud
-        t = rospy.Time(secs=msg.header.stamp.secs, nsecs=msg.header.stamp.nsecs)
+        ti = rospy.Time(secs=msg.header.stamp.secs, nsecs=msg.header.stamp.nsecs)
+        self._last_t = ti
         try:
-            transform = self._tf_buffer.lookup_transform(self.map_frame, msg.header.frame_id, t, rospy.Duration(1.0))
+            transform = self._tf_buffer.lookup_transform(self.map_frame, msg.header.frame_id, ti, rospy.Duration(1.0))
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            print(e)
+            print("pointcloud_callback error:", e)
             return
 
         t = transform.transform.translation
-        t = np.array( [t.x, t.y, t.z] )
+        t = np.array([t.x, t.y, t.z])
         q = transform.transform.rotation
-        R = quaternion_matrix([q.w, q.x, q.y, q.z])[:3,:3]
+        R = quaternion_matrix([q.x, q.y, q.z, q.w])[:3, :3]
 
         # process pointcloud
         self._map.input(pts, channels, R, t, 0, 0)
         self._pointcloud_process_counter += 1
         print(self._pointcloud_process_counter)
-        
+
     def update_pose(self, t):
         # get pose of base
-        t = rospy.Time.now()
+        # t = rospy.Time.now()
+        if self._last_t is None:
+            return
         try:
-            transform = self._tf_buffer.lookup_transform(self.map_frame, self.base_frame, t, rospy.Duration(1.0))
+            transform = self._tf_buffer.lookup_transform(
+                self.map_frame, self.base_frame, self._last_t, rospy.Duration(1.0)
+            )
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            print(e)
-            return        
+            print("update_pose error: ", e)
+            return
         t = transform.transform.translation
-        trans = np.array( [t.x, t.y, t.z] )
+        trans = np.array([t.x, t.y, t.z])
         q = transform.transform.rotation
-        rot = quaternion_matrix([q.w, q.x, q.y, q.z])[:3,:3]
+        rot = quaternion_matrix([q.x, q.y, q.z, q.w])[:3, :3]
         self._map.move_to(trans, rot)
-        
+
         self._map_t = t
         self._map_q = q
-        
+
     def update_variance(self, t):
         self._map.update_variance()
-    
+
     def update_time(self, t):
         self._map.update_time()
-    
+
     def get_ros_params(self):
         # TODO fix this here when later launching with launch-file
         # This is currently {p} elevation_mapping")
@@ -199,10 +234,11 @@ class ElevationMapWrapper():
         self.enable_normal_arrow_publishing = rospy.get_param("~enable_normal_arrow_publishing", False)
         self.enable_drift_corrected_TF_publishing = rospy.get_param("~enable_drift_corrected_TF_publishing", False)
         self.use_initializer_at_start = rospy.get_param("~use_initializer_at_start", False)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     emw = ElevationMapWrapper()
-    
+
     while not rospy.is_shutdown():
         try:
             rospy.spin()