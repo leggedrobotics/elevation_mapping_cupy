2e36f825be69741b2ab34cd55a2e9e626e67059b
Takahiro <takahiro.miki1992@gmail.com>
2023-11-23 18:43:50 +0100

    Added Dockerfile.x86 for building a Docker image with CUDA and PyTorch support. Added build.sh script for building the Docker image. Added run.sh script for launching a container from the Docker image. Modified turtlesim_example.launch to accept an argument for the RViz configuration file. Modified turtlesim_init.launch to include the turtlebot3_gazebo launch file and spawn the turtlebot3 model. Modified requirements.txt to comment out the cupy package.

---
+++ docker/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+IMAGE_NAME="elevation_mapping_cupy:latest"
+
+# Define environment variables for enabling graphical output for the container.
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    touch $XAUTH
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+    chmod a+r $XAUTH
+fi
+
+#==
+# Launch container
+#==
+
+# Create symlinks to user configs within the build context.
+mkdir -p .etc && cd .etc
+ln -sf /etc/passwd .
+ln -sf /etc/shadow .
+ln -sf /etc/group .
+cd ..
+
+# Launch a container from the prebuilt image.
+echo "---------------------"
+RUN_COMMAND="docker run \
+  --volume=$XSOCK:$XSOCK:rw \
+  --volume=$XAUTH:$XAUTH:rw \
+  --env="QT_X11_NO_MITSHM=1" \
+  --env="XAUTHORITY=$XAUTH" \
+  --env="DISPLAY=$DISPLAY" \
+  --ulimit rtprio=99 \
+  --cap-add=sys_nice \
+  --privileged \
+  --net=host \
+  -eHOST_USERNAME=$(whoami) \
+  -v$HOME:$HOME \
+  -v$(pwd)/.etc/shadow:/etc/shadow \
+  -v$(pwd)/.etc/passwd:/etc/passwd \
+  -v$(pwd)/.etc/group:/etc/group \
+  -v/media:/media \
+  --gpus all \
+  -it $IMAGE_NAME"
+echo -e "[run.sh]: \e[1;32mThe final run command is\n\e[0;35m$RUN_COMMAND\e[0m."
+$RUN_COMMAND
+echo -e "[run.sh]: \e[1;32mDocker terminal closed.\e[0m"
+#   --entrypoint=$ENTRYPOINT \
