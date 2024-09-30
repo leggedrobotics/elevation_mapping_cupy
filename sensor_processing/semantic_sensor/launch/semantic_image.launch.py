6d2c1b40253ba50f7d42da1e06b25b18f1ce3bf5
Idate96 <lorenzoterenzi96@gmail.com>
2024-09-30 10:32:47 +0200

    semantic sensor ros2

---
+++ sensor_processing/semantic_sensor/launch/semantic_image.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='semantic_sensor',
+            executable='image_node.py',
+            name='semantic_image',
+            arguments=['front_cam_image'],
+            output='screen',
+            parameters=[{'ros__parameters': 'config/sensor_parameter.yaml'}]
+        )
+    ])
---
+++ sensor_processing/semantic_sensor/launch/semantic_pointcloud.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='semantic_sensor',
+            executable='pointcloud_node.py',
+            name='semantic_pointcloud',
+            arguments=['front_cam'],
+            output='screen',
+            parameters=[{'ros__parameters': 'config/sensor_parameter.yaml'}]
+        )
+    ])
